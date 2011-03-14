cf011b85e5eb52c8dc31355912f2d100c4dd0dec
Matthew Dunlap <mattchristiandunlap@gmail.com>
2011-03-13 23:05:11 -0400

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  #Return Logo
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
